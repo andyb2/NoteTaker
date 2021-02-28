674d08d95f3db16ad0fde852283a4059b55bfda0
AMB <a.michael.boyle@gmail.com>
2021-02-28 13:56:24 -0500

    removed some whitespace

--- Develop/server.js
+++ Develop/server.js
@@ -4,11 +4,9 @@ const fs = require('fs')
 const uuid = require('node-uuid')
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
 
 let noteList = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 
