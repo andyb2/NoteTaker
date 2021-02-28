5c0bd12de76e0f4cb50dd339c0aa485af207d407
AMB <a.michael.boyle@gmail.com>
2021-02-28 13:59:49 -0500

    added comments

--- Develop/server.js
+++ Develop/server.js
@@ -10,10 +10,12 @@ app.use(express.json());
 
 let noteList = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 
+// Sends server notelist request
 app.get('/api/notes', (req, res)=>{
     res.send(noteList)
 });
 
+// Sends saved newly saved notes to server to be saved
 app.post('/api/notes', (req, res)=>{
     let savedNotes = req.body
     console.log(req.body)
@@ -24,6 +26,7 @@ app.post('/api/notes', (req, res)=>{
     res.json(noteList)
 });
 
+// Sends a delete request to remove a saved note
 app.delete(`/api/notes/:id`, (req, res)=>{
     const removal = req.params.id;
     noteList = noteList.filter(item => item.id !== removal)
